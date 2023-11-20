33ca437ddaa720557772becf897b7f82fdce9293
Nishkarsh Gautam <23m2114@iitb.ac.in>
2023-11-20 17:43:39 +0530

    added py files

--- shopSizzle/shop/admin.py
+++ shopSizzle/shop/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from shop.models import *
 
 # Register your models here.
+from .models import Product, Contact, Orders, OrderUpdate
 
 admin.site.register(Product)
+admin.site.register(Contact)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
